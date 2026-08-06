94eecee48c5deab999bbe9b586ad23f01cb0e0bf
MrAnyx <gentle.job6107@fastmail.com>
2026-08-06 14:32:10 +0200

    feat: Implement search and details procedures for TMDB movies with error handling

--- server/trpc/routers/tmdb.ts
+++ server/trpc/routers/tmdb.ts
@@ -4,97 +4,110 @@ import * as z from "zod";
 import { router, protectedProcedure } from "#server/trpc/init";
 
 export default router({
-	// search: protectedProcedure
-	// 	.input(
-	// 		z.object({
-	// 			search: ServerPaginationValidation.search,
-	// 			page: ServerPaginationValidation.page,
-	// 		}),
-	// 	)
-	// 	.output(PaginatedSchema(TmdbMovieSearchDefaultViewSchema))
-	// 	.query(async ({ input, ctx }) => {
-	// 		try {
-	// 			if (!input.search) {
-	// 				return {
-	// 					total: 0,
-	// 					results: [],
-	// 				};
-	// 			}
-	// 			const tmdbMovies = await tmdb("/search/movie", {
-	// 				schema: TmdbMovieSearchResponseSchema,
-	// 				query: {
-	// 					query: input.search,
-	// 					page: input.page,
-	// 				},
-	// 			});
-	// 			const externalIds = tmdbMovies.results.map((x) => x.id);
-	// 			const myMovies = await prisma.movie.findMany({
-	// 				where: {
-	// 					ownerId: ctx.user.id,
-	// 					externalId: {
-	// 						in: externalIds,
-	// 					},
-	// 				},
-	// 				select: {
-	// 					id: true,
-	// 					externalId: true,
-	// 				},
-	// 			});
-	// 			const myMoviesMap = new Map(myMovies.map((m) => [m.externalId, m.id]));
-	// 			const movies = tmdbMovies.results.map((movie) => ({
-	// 				...movie,
-	// 				internalId: myMoviesMap.get(movie.id),
-	// 			}));
-	// 			return {
-	// 				total: tmdbMovies.total_results,
-	// 				results: movies,
-	// 			};
-	// 		} catch (err: any) {
-	// 			throw new TRPCError({
-	// 				code: "BAD_REQUEST",
-	// 				cause: err,
-	// 				message: "An error occured while getting movies",
-	// 			});
-	// 		}
-	// 	}),
-	// details: protectedProcedure
-	// 	.input(
-	// 		z.object({
-	// 			id: ServerTmdbMovieValidation.id,
-	// 		}),
-	// 	)
-	// 	.output(TmdbMovieDetailsDefaultViewSchema)
-	// 	.query(async ({ input }) => {
-	// 		try {
-	// 			return await tmdb(`/movie/${input.id}`, {
-	// 				schema: TmdbMovieDetailsResponseSchema,
-	// 			});
-	// 		} catch (err: any) {
-	// 			throw new TRPCError({
-	// 				code: "BAD_REQUEST",
-	// 				cause: err,
-	// 				message: "An error occured while getting movies",
-	// 			});
-	// 		}
-	// 	}),
-	// credits: protectedProcedure
-	// 	.input(
-	// 		z.object({
-	// 			id: ServerTmdbMovieValidation.id,
-	// 		}),
-	// 	)
-	// 	.output(TmdbMovieCreditsDefaultViewSchema)
-	// 	.query(async ({ input }) => {
-	// 		try {
-	// 			return await tmdb(`/movie/${input.id}/credits`, {
-	// 				schema: TmdbMovieCreditsResponseSchema,
-	// 			});
-	// 		} catch (err: any) {
-	// 			throw new TRPCError({
-	// 				code: "BAD_REQUEST",
-	// 				cause: err,
-	// 				message: "An error occured while getting movie credits",
-	// 			});
-	// 		}
-	// 	}),
+	search: protectedProcedure
+		.input(
+			z.object({
+				search: ServerPaginationValidation.search,
+				page: ServerPaginationValidation.page,
+			}),
+		)
+		.output(PaginatedSchema(TmdbMovieSearchDefaultViewSchema))
+		.query(async ({ input, ctx }) => {
+			try {
+				if (!input.search) {
+					return {
+						total: 0,
+						results: [],
+					};
+				}
+
+				const tmdbMovies = await tmdb("/search/movie", {
+					schema: TmdbMovieSearchResponseSchema,
+					query: {
+						query: input.search,
+						page: input.page,
+					},
+				});
+
+				const externalIds = tmdbMovies.results.map((x) => x.id);
+
+				const myMovies = await prisma.movie.findMany({
+					where: {
+						media: {
+							ownerId: ctx.user.id,
+							externalId: {
+								in: externalIds,
+							},
+						},
+					},
+					select: {
+						id: true,
+						media: {
+							select: {
+								externalId: true,
+							},
+						},
+					},
+				});
+
+				const myMoviesMap = new Map(myMovies.map((m) => [m.media.externalId, m.id]));
+
+				const movies = tmdbMovies.results.map((movie) =>
+					Object.assign(movie, { internalId: myMoviesMap.get(movie.id) }),
+				);
+
+				return {
+					total: tmdbMovies.total_results,
+					results: movies,
+				};
+			} catch (err: any) {
+				throw new TRPCError({
+					code: "BAD_REQUEST",
+					cause: err,
+					message: "An error occured while getting movies",
+				});
+			}
+		}),
+
+	details: protectedProcedure
+		.input(
+			z.object({
+				id: ServerTmdbMovieValidation.id,
+			}),
+		)
+		.output(TmdbMovieDetailsDefaultViewSchema)
+		.query(async ({ input }) => {
+			try {
+				return await tmdb(`/movie/${input.id}`, {
+					schema: TmdbMovieDetailsResponseSchema,
+				});
+			} catch (err: any) {
+				throw new TRPCError({
+					code: "BAD_REQUEST",
+					cause: err,
+					message: "An error occured while getting movies",
+				});
+			}
+		}),
+
+	credits: protectedProcedure
+		.input(
+			z.object({
+				id: ServerTmdbMovieValidation.id,
+			}),
+		)
+		.output(TmdbMovieCreditsDefaultViewSchema)
+		.query(async ({ input }) => {
+			try {
+				return await tmdb(`/movie/${input.id}/credits`, {
+					schema: TmdbMovieCreditsResponseSchema,
+				});
+			} catch (err: any) {
+				throw new TRPCError({
+					code: "BAD_REQUEST",
+					cause: err,
+					message: "An error occured while getting movie credits",
+				});
+			}
+		}),
 });
