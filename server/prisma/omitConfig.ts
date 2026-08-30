3e30c26d3ab8f8216a84c446b2672b06e8e2b5ef
MrAnyx <gentle.job6107@fastmail.com>
2026-08-30 18:37:12 +0200

    refactor: remove omitConfig file and related PrismaClient options

--- server/prisma/omitConfig.ts
+++
@@ -1,5 +0,0 @@
-import { PrismaClient } from "#prisma/client";
-
-type ClientOptions = NonNullable<ConstructorParameters<typeof PrismaClient>[0]>;
-
-export const omitConfig = {} satisfies ClientOptions["omit"];
