f515bd469119098f0f502660676173d110726abd
MrAnyx <gentle.job6107@fastmail.com>
2026-09-21 23:03:09 +0200

    refactor: restructure series and season pages; remove old implementations and add new index files

--- app/stores/serie.ts
+++ app/stores/serie.ts
@@ -21,13 +21,16 @@ export const useSerieStore = defineStore("serie", {
 
 		async createSerieFromExternal(payload: TRPCProcedureInput<"serie", "createFromExternal">) {
 			const trpc = useTrpc();
+
 			const serie = await trpc.serie.createFromExternal.mutate(payload);
 			this.total += 1;
+
 			return serie;
 		},
 
 		async deleteSerie(payload: TRPCProcedureInput<"serie", "delete">) {
 			const trpc = useTrpc();
+
 			await trpc.serie.delete.mutate(payload);
 			this.total -= 1;
 		},
